e472a6010761d3eda75842f806e63b19223a733d
ecorreia design <el4591@hotmail.com>
2017-07-08 15:29:19 -0400

    Simple Database Class with built-in query function

---
+++ config.php
@@ -0,0 +1,7 @@
+<?php
+	
+	!defined("Server") ? define("Host", "localhost") : null;
+	!defined("User") ? define("User","root") : null;
+	!defined("Pass") ? define("Pass","") : null;
+	!defined("DB") ? define("DB","test") : null;
+	
